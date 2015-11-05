2095f41886aa553b12f6cbfbcd0bd738c4616d81
Joaquin Ampuero <joaquin.ampuero.r@gmail.com>
2015-11-05 18:03:08 -0300

    se agrego hero transitions y coordinator layout con toolbar

--- app/src/main/java/cl/continuum/stcworkshop/ExpositorDetailActivity.java
+++ app/src/main/java/cl/continuum/stcworkshop/ExpositorDetailActivity.java
@@ -1,7 +1,10 @@
 package cl.continuum.stcworkshop;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,25 +12,35 @@ public class ExpositorDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        requestWindowFeature( Window.FEATURE_NO_TITLE);
+        super.onCreate ( savedInstanceState );
         setContentView(R.layout.activity_expositor_detail);
         int position = getIntent().getExtras().getInt("position");
 
         ImageView expositorImage = (ImageView) findViewById(R.id.expositor_image);
         TextView expositorName = (TextView) findViewById(R.id.expositor_name);
+        //aquí añadimos el trasition name a la imagen
+
+        if( Build.VERSION.SDK_INT>=21){
+            expositorImage.setTransitionName ( "transitionImage" );
+        }
+
         TextView expositorDescription = (TextView) findViewById(R.id.expositor_description);
 
-        int id = getBaseContext().getResources().getIdentifier("expositor_"+(position+1), "drawable", getBaseContext().getPackageName());
-        expositorImage.setImageResource(id);
+        int id = getBaseContext().getResources().getIdentifier("expositor_"+(position+1), "drawable", getBaseContext().getPackageName ( ));
+        expositorImage.setImageResource ( id );
 
         String name = getResources().getStringArray(R.array.expositors_names)[position];
         expositorName.setText(name);
 
-        expositorDescription.setText(getResources().getStringArray(R.array.expositors_desc)[position]);
+        expositorDescription.setText ( getResources ( ).getStringArray ( R.array.expositors_desc )[ position ] );
 
-        getSupportActionBar().show();
-        getSupportActionBar().setTitle(name);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Toolbar toolbar= (Toolbar)findViewById ( R.id.expositor_toolbar );
+        setSupportActionBar ( toolbar );
+        getSupportActionBar ( ).setTitle ( " " );
+        getSupportActionBar().setDisplayHomeAsUpEnabled ( true );
+
 
     }
 }
--- app/src/main/java/cl/continuum/stcworkshop/MainActivity.java
+++ app/src/main/java/cl/continuum/stcworkshop/MainActivity.java
@@ -1,8 +1,12 @@
 package cl.continuum.stcworkshop;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,69 +18,91 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     ExpositorsListAdapter mAdapter;
 
-    AdapterView.OnItemClickListener expositorDetail = new AdapterView.OnItemClickListener() {
+    AdapterView.OnItemClickListener expositorDetail = new AdapterView.OnItemClickListener ( )
+    {
+
         @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Intent intent = new Intent(MainActivity.this, ExpositorDetailActivity.class);
-            Bundle mBundle = new Bundle();
-            mBundle.putInt("position", position);
-            intent.putExtras(mBundle);
-            startActivity(intent);
+        public void onItemClick ( AdapterView< ? > parent, View view, int position, long id )
+        {
+            Intent intent = new Intent ( MainActivity.this, ExpositorDetailActivity.class );
+            Bundle mBundle = new Bundle ( );
+            mBundle.putInt ( "position", position );
+            intent.putExtras ( mBundle );
+
+            //verificamos si la versión del sdk es mayor o igual a 21
+            if ( Build.VERSION.SDK_INT >= 21 )
+            {
+                View sharedImage = view.findViewById ( R.id.expositor_image );
+                ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation
+                        ( MainActivity.this, sharedImage,"transitionImage" );
+                startActivity ( intent, transitionActivityOptions.toBundle ( ) );
+            } else
+            {
+                startActivity ( intent );
+            }
         }
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate ( Bundle savedInstanceState )
+    {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
 
-        mAdapter = new ExpositorsListAdapter(this);
-        ListView list = (ListView)findViewById(R.id.expositors);
-        list.setAdapter(mAdapter);
-        getSupportActionBar().show();
-        getSupportActionBar().setTitle(getResources().getString(R.string.expositors_act));
+        mAdapter = new ExpositorsListAdapter ( this );
+        ListView list = ( ListView ) findViewById ( R.id.expositors );
+        list.setAdapter ( mAdapter );
+        getSupportActionBar ( ).show ( );
+        getSupportActionBar ( ).setTitle ( getResources ( ).getString ( R.string.expositors_act ) );
 
-        list.setOnItemClickListener(expositorDetail);
+        list.setOnItemClickListener ( expositorDetail );
     }
 
-    private class ExpositorsListAdapter extends BaseAdapter {
+    private class ExpositorsListAdapter extends BaseAdapter
+    {
 
-        private String[] expositorsNames = getResources().getStringArray(R.array.expositors_names);
+        private String[] expositorsNames = getResources ( ).getStringArray ( R.array.expositors_names );
         private LayoutInflater inflater = null;
 
-        public ExpositorsListAdapter(Activity activity){
-            inflater = (LayoutInflater) activity.getSystemService((Context.LAYOUT_INFLATER_SERVICE));
+        public ExpositorsListAdapter ( Activity activity )
+        {
+            inflater = ( LayoutInflater ) activity.getSystemService ( ( Context.LAYOUT_INFLATER_SERVICE ) );
         }
 
         @Override
-        public int getCount() {
+        public int getCount ( )
+        {
             return expositorsNames.length;
         }
 
         @Override
-        public Object getItem(int position) {
-            return expositorsNames[position];
+        public Object getItem ( int position )
+        {
+            return expositorsNames[ position ];
         }
 
         @Override
-        public long getItemId(int position) {
+        public long getItemId ( int position )
+        {
             return position;
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView ( int position, View convertView, ViewGroup parent )
+        {
             View view = convertView;
-            if (convertView == null)
-                view = inflater.inflate(R.layout.expositor_cell, null);
-            TextView name = (TextView) view.findViewById(R.id.expositor_name);
-            ImageView image = (ImageView) view.findViewById(R.id.expositor_image);
-            name.setText(expositorsNames[position]);
-            int id = getBaseContext().getResources().getIdentifier("expositor_"+(position+1), "drawable", getBaseContext().getPackageName());
-            image.setImageResource(id);
+            if ( convertView == null )
+                view = inflater.inflate ( R.layout.expositor_cell, null );
+            TextView name = ( TextView ) view.findViewById ( R.id.expositor_name );
+            ImageView image = ( ImageView ) view.findViewById ( R.id.expositor_image );
+            name.setText ( expositorsNames[ position ] );
+            int id = getBaseContext ( ).getResources ( ).getIdentifier ( "expositor_" + ( position + 1 ), "drawable", getBaseContext ( ).getPackageName ( ) );
+            image.setImageResource ( id );
             return view;
         }
     }
